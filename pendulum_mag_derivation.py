7c1d788b71dc0c55c2f73c3b7376c2652b1bdc05
Louis Kamp Eskildsen <louiskampeskildsen99@gmail.com>
2025-01-08 23:43:02 +0100

    Working with multiple magnets

--- pendulum_mag_derivation.py
+++ pendulum_mag_derivation.py
@@ -5,36 +5,37 @@ phi = Function('phi')(t)
 phi_dot = diff(phi,t)
 
 # %%
-l = 1
-mu_0 = 1
-M_p = 1
-g = 1
-alpha = 0.5
+
+l, mu_0, M_p, g, alpha = symbols("l, mu_0, M_p, g, alpha", real=True)
 
 x_p = l * cos(phi)
 y_p = l * sin(phi)
 
-x_1 = 1
-y_1 = 1
+U_g =  -M_p * g *x_p
+# %%
 
-m_p = 1*Matrix([cos(phi), sin(phi)])
-m_1 = 0.5*Matrix([1,1])
+x_1, y_1 = symbols("x_1, y_1")
+m_1_x, m_1_y = symbols("m_1_x, m_1_y")
 
-r = Matrix([ x_p - x_1, y_p - y_1 ])
-r_norm = sqrt( r[0]**2 + r[1]**2 )
+r = Matrix([x_p - x_1, y_p - y_1])
+r_norm = sqrt(r[0]**2 + r[1]**2)
 r_hat = r / r_norm
 
-B_1 = (mu_0 / 4*pi) * ((3*r_hat * ( r_hat.T @ m_1 ) - m_1) / (r_norm**3))
-U_1 = simplify((-m_p.T @ B_1)[0])
-U_g =  M_p * g * l*(l-x_p)
-# %%
-V = U_g + U_1
+m_1 = Matrix([m_1_x,m_1_y])
+m_p = Matrix([cos(phi), sin(phi)])
+
+B_1 = (mu_0 / 4*pi)*( 3*r_hat * r_hat.dot(m_1) - m_1 ) / (r_norm**3)
+U_1 = simplify(-m_p.dot(B_1))
+
+#%%
+V = U_g + 0*U_1
 T = 0.5 * M_p * ( diff(x_p,t)**2 + diff(y_p,t)**2 )
 L = simplify(T - V)
 
 # %%
 
-sol = simplify(Eq(diff(L, phi) - diff(diff(L, phi_dot),t), alpha*phi_dot))
+sol = Eq(diff(L, phi) - diff(diff(L, phi_dot),t), 0*alpha*phi_dot)
+#%%
 sol
 # %%
 phi_t_t = solve(sol, diff(phi,t,t))[0]
@@ -42,4 +43,11 @@ phi_t_t = solve(sol, diff(phi,t,t))[0]
 # %%
 
 phi_t_t_func = lambdify([phi, phi_dot], phi_t_t, modules="numpy")
+# %%
+from sympy.printing.pycode import pycode
+
+phi_1, d_phi = symbols("phi d_{phi}")
+
+pycode(phi_t_t.subs({phi: phi_1, phi_dot: d_phi}))
+
 # %%
--- pendulum_mag_sim.py
+++ pendulum_mag_sim.py
@@ -1,37 +1,94 @@
+#%%
+from dataclasses import dataclass
+import typing
 from scipy.integrate import solve_ivp
 import numpy as np
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
-from pendulum_mag_derivation import phi_t_t_func
 
-def f(t, y):
-    return np.array([y[1], phi_t_t_func(y[0], y[1])])
+@dataclass
+class Magnet():
+    position: np.ndarray    # [x,y]-position in modified coordinate system
+    moment: np.ndarray      # magnetic moment [x,y]
 
-ts = np.linspace(0,30,500)
-sol = solve_ivp(f, (0,30), np.array([0,0]), t_eval=ts)
-phis  = sol.y[0]
+class Pendulum():
+    M_p: float      # Pendulum mass
+    l: float        # Pendulum length
+    mu_0: float     # permeability of free space
+    g: float        # gravitational acceleration
+    alpha: float    # dampening constant
 
-xs = np.cos(phis)
-ys = np.sin(phis)
+    magnets: list[Magnet]
 
-fig, ax = plt.subplots()
-line, = ax.plot([], [], 'o-', lw=2)
-ax.set_xlim(-1.5, 1.5)
-ax.set_ylim(-1.5, 1.5)
-ax.invert_yaxis()
+    def __init__(self, M_p: float, l: float, mu_0: float, g: float, alpha: float):
+        self.M_p = M_p
+        self.l = l
+        self.mu_0 = mu_0
+        self.g = g
+        self.alpha = alpha
+        self.magnets = []
 
-def init():
-    line.set_data([], [])
-    return line,
+    def add_magnet(self, magnet: Magnet):
+        self.magnets.append(magnet)
 
-def update(frame):
-    line.set_data([0, ys[frame]], [0, xs[frame]])
-    return line,
-    
-ani = FuncAnimation(fig, update, frames=len(ts), init_func=init, blit=True)
-plt.grid()
-plt.xlabel("y")
-plt.ylabel("x")
-ani = FuncAnimation(fig, update, frames=len(ts), init_func=init, blit=True, interval=10)
-ax.set_aspect('equal')
-plt.show()
+    def simulate(self, time_interval: tuple[float,float], initial_condition: tuple[float, float], t_num_eval=None):
+        if t_num_eval == None:
+            t_num_eval = 10*(time_interval[1] - time_interval[0])
+        
+        # setup
+        f = self.create_f()
+        ts = np.linspace(time_interval[0],time_interval[1],t_num_eval)
+
+        # solve
+        sol = solve_ivp(f, (time_interval[0],time_interval[1]), np.array([initial_condition[0],initial_condition[1]]), t_eval=ts)
+        phis  = sol.y[0]
+        d_phis = sol.y[1]
+
+        xs = np.cos(phis)
+        ys = np.sin(phis)
+
+        # plot
+        fig, ax = plt.subplots()
+        line, = ax.plot([], [], 'o-', lw=2)
+        ax.set_xlim(-2, 2)
+        ax.set_ylim(-2, 2)
+        ax.invert_yaxis()
+
+        for magnet in self.magnets:
+            ax.scatter([magnet.position[1]], [magnet.position[0]])
+            ax.quiver(magnet.position[1], magnet.position[0], magnet.moment[1], magnet.moment[0], angles='xy', scale_units='xy', scale=1)
+
+        def init():
+            line.set_data([], [])
+            return line,
+
+        def update(frame):
+            line.set_data([0, ys[frame]], [0, xs[frame]])
+            return line,
+            
+        ani = FuncAnimation(fig, update, frames=len(ts), init_func=init, blit=True)
+        plt.grid()
+        plt.xlabel("y")
+        plt.ylabel("x")
+        ani = FuncAnimation(fig, update, frames=len(ts), init_func=init, blit=True, interval=10)
+        ax.set_aspect('equal')
+        plt.show()
+
+    def create_f(self):
+        def f(t:float,y:np.ndarray) -> np.ndarray:
+            sol_g = -self.g*np.sin(y[0]) / self.l
+            sol_m = np.sum([
+                0.0694200459087245*self.mu_0*(-2.0*self.l**4*magnet.moment[0]*np.sin(y[0]) + 2.0*self.l**4*magnet.moment[1]*np.cos(y[0]) - 2.0*self.l**3*magnet.moment[0]*magnet.position[0]*np.sin(2.0*y[0]) + 2.0*self.l**3*magnet.moment[0]*magnet.position[1]*np.cos(2.0*y[0]) + 7.0*self.l**3*magnet.moment[0]*magnet.position[1] + 2.0*self.l**3*magnet.moment[1]*magnet.position[0]*np.cos(2.0*y[0]) - 7.0*self.l**3*magnet.moment[1]*magnet.position[0] + 2.0*self.l**3*magnet.moment[1]*magnet.position[1]*np.sin(2.0*y[0]) + 11.25*self.l**2*magnet.moment[0]*magnet.position[0]**2*np.sin(y[0]) + 0.25*self.l**2*magnet.moment[0]*magnet.position[0]**2*np.sin(3.0*y[0]) - 14.5*self.l**2*magnet.moment[0]*magnet.position[0]*magnet.position[1]*np.cos(y[0]) - 0.5*self.l**2*magnet.moment[0]*magnet.position[0]*magnet.position[1]*np.cos(3.0*y[0]) - 3.25*self.l**2*magnet.moment[0]*magnet.position[1]**2*np.sin(y[0]) - 0.25*self.l**2*magnet.moment[0]*magnet.position[1]**2*np.sin(3.0*y[0]) + 3.25*self.l**2*magnet.moment[1]*magnet.position[0]**2*np.cos(y[0]) - 0.25*self.l**2*magnet.moment[1]*magnet.position[0]**2*np.cos(3.0*y[0]) + 14.5*self.l**2*magnet.moment[1]*magnet.position[0]*magnet.position[1]*np.sin(y[0]) - 0.5*self.l**2*magnet.moment[1]*magnet.position[0]*magnet.position[1]*np.sin(3.0*y[0]) - 11.25*self.l**2*magnet.moment[1]*magnet.position[1]**2*np.cos(y[0]) + 0.25*self.l**2*magnet.moment[1]*magnet.position[1]**2*np.cos(3.0*y[0]) - 2.0*self.l*magnet.moment[0]*magnet.position[0]**3*np.sin(2.0*y[0]) + 6.5*self.l*magnet.moment[0]*magnet.position[0]**2*magnet.position[1]*np.cos(2.0*y[0]) - 3.5*self.l*magnet.moment[0]*magnet.position[0]**2*magnet.position[1] + 7.0*self.l*magnet.moment[0]*magnet.position[0]*magnet.position[1]**2*np.sin(2.0*y[0]) - 2.5*self.l*magnet.moment[0]*magnet.position[1]**3*np.cos(2.0*y[0]) - 3.5*self.l*magnet.moment[0]*magnet.position[1]**3 - 2.5*self.l*magnet.moment[1]*magnet.position[0]**3*np.cos(2.0*y[0]) + 3.5*self.l*magnet.moment[1]*magnet.position[0]**3 - 7.0*self.l*magnet.moment[1]*magnet.position[0]**2*magnet.position[1]*np.sin(2.0*y[0]) + 6.5*self.l*magnet.moment[1]*magnet.position[0]*magnet.position[1]**2*np.cos(2.0*y[0]) + 3.5*self.l*magnet.moment[1]*magnet.position[0]*magnet.position[1]**2 + 2.0*self.l*magnet.moment[1]*magnet.position[1]**3*np.sin(2.0*y[0]) - 2.0*magnet.moment[0]*magnet.position[0]**4*np.sin(y[0]) + 3.0*magnet.moment[0]*magnet.position[0]**3*magnet.position[1]*np.cos(y[0]) - magnet.moment[0]*magnet.position[0]**2*magnet.position[1]**2*np.sin(y[0]) + 3.0*magnet.moment[0]*magnet.position[0]*magnet.position[1]**3*np.cos(y[0]) + magnet.moment[0]*magnet.position[1]**4*np.sin(y[0]) - magnet.moment[1]*magnet.position[0]**4*np.cos(y[0]) - 3.0*magnet.moment[1]*magnet.position[0]**3*magnet.position[1]*np.sin(y[0]) + magnet.moment[1]*magnet.position[0]**2*magnet.position[1]**2*np.cos(y[0]) - 3.0*magnet.moment[1]*magnet.position[0]*magnet.position[1]**3*np.sin(y[0]) + 2.0*magnet.moment[1]*magnet.position[1]**4*np.cos(y[0]))/(self.M_p*self.l**2*(0.5*self.l**2 - self.l*magnet.position[0]*np.cos(y[0]) - self.l*magnet.position[1]*np.sin(y[0]) + 0.5*magnet.position[0]**2 + 0.5*magnet.position[1]**2)**(7/2))
+                for magnet in self.magnets
+            ])
+            sol_alpha = -(self.alpha * y[1]) / (self.M_p * self.l**2)
+            return np.array([y[1], sol_g + sol_alpha + sol_m])
+
+        return f
+# %%
+sim = Pendulum(1,1,1,1,0.1)
+sim.add_magnet(Magnet(np.array([1.5,1]), np.array([0.5,0])))
+sim.add_magnet(Magnet(np.array([1.5,-1]), np.array([-0.5,0])))
+# %%
+sim.simulate((0,50), (0,1))
+# %%
