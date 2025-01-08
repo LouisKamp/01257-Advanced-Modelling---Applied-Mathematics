3f2465873ba83a3fa37cfc67b508495bfa37a682
Louis Kamp Eskildsen <louiskampeskildsen99@gmail.com>
2025-01-08 14:45:09 +0100

    New derivation

--- pendulum_mag_derivation.py
+++ pendulum_mag_derivation.py
@@ -5,46 +5,41 @@ phi = Function('phi')(t)
 phi_dot = diff(phi,t)
 
 # %%
-M_p = 1
-g = 0*2.7
 l = 1
 mu_0 = 1
-m_p = Matrix([sin(phi), cos(phi)])
-m_1 = 10*Matrix([-1,-1])
+M_p = 1
+g = 1
+alpha = 0.1
 
-# pendulum placement
-x_p = l*sin(phi)
-y_p = -l*cos(phi)
+x_p = l * cos(phi)
+y_p = l * sin(phi)
 
-# magnetic 1 placement
-x_1 = -1
-y_1 = -1
+x_1 = 1
+y_1 = 1
 
+m_p = Matrix([cos(phi), sin(phi)])
+m_1 = 1*Matrix([1,1])
 
-r_1 = Matrix([x_p - x_1, y_p - y_1])
-r_1_norm = sqrt( r_1[0]**2 + r_1[1]**2 )
-r_1_hat = r_1 / r_1_norm
+r = Matrix([ x_p - x_1, y_p - y_1 ])
+r_norm = sqrt( r[0]**2 + r[1]**2 )
+r_hat = r / r_norm
+
+B_1 = (mu_0 / 4*pi) * ((3*r_hat * ( r_hat.T @ m_1 ) - m_1) / (r_norm**3))
+U_1 = simplify((-m_p.T @ B_1)[0])
+U_g =  M_p * g * l*(l-x_p)
+# %%
+V = U_g + U_1
+T = 0.5 * M_p * ( diff(x_p,t)**2 + diff(y_p,t)**2 )
+L = simplify(T - V)
 
 # %%
-B_1 = (mu_0 / (4 * pi)) * (3*r_1_hat * ( r_1_hat.T @ m_1 ) - m_1) / (r_1_norm**3)
-U_1 = simplify(-m_p.T @ B_1)[0]
 
-# %%
-T = simplify(0.5*M_p*( diff(x_p, t)**2 + diff(y_p,t)**2))
-
-# %%
-V = M_p*g*y_p + U_1
-
-# %%
-L = T - V
-
-# %%
-sol = Eq(diff(L, phi) - diff(diff(L, phi_dot),t),0)
-
+sol = simplify(Eq(diff(L, phi) - diff(diff(L, phi_dot),t), 0))
+sol
 # %%
 phi_t_t = solve(sol, diff(phi,t,t))[0]
 
 # %%
 
 phi_t_t_func = lambdify(phi, phi_t_t, modules="numpy")
-
+# %%
--- pendulum_mag_sim.py
+++ pendulum_mag_sim.py
@@ -8,29 +8,30 @@ def f(t, y):
     return np.array([y[1], phi_t_t_func(y[0])])
 
 ts = np.linspace(0,30,500)
-sol = solve_ivp(f, (0,30), np.array([np.pi/8,0]), t_eval=ts)
+sol = solve_ivp(f, (0,30), np.array([np.pi/4,0]), t_eval=ts)
 phis  = sol.y[0]
 
-xs = 1*np.sin(phis)
-ys = -1*np.cos(phis)
+xs = np.cos(phis)
+ys = np.sin(phis)
 
 fig, ax = plt.subplots()
 line, = ax.plot([], [], 'o-', lw=2)
 ax.set_xlim(-1.5, 1.5)
 ax.set_ylim(-1.5, 1.5)
+ax.invert_yaxis()
 
 def init():
     line.set_data([], [])
     return line,
 
 def update(frame):
-    line.set_data([0, xs[frame]], [0, ys[frame]])
+    line.set_data([0, ys[frame]], [0, xs[frame]])
     return line,
-
+    
 ani = FuncAnimation(fig, update, frames=len(ts), init_func=init, blit=True)
 plt.grid()
-plt.xlabel("x")
-plt.ylabel("y")
+plt.xlabel("y")
+plt.ylabel("x")
 ani = FuncAnimation(fig, update, frames=len(ts), init_func=init, blit=True, interval=10)
 ax.set_aspect('equal')
 plt.show()
