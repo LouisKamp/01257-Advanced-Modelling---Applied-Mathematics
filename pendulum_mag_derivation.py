d09ac214cb5936591a20c2bb386ab475b23905cd
Louis Kamp Eskildsen <louiskampeskildsen99@gmail.com>
2025-01-08 15:10:52 +0100

    Energy loss

--- pendulum_mag_derivation.py
+++ pendulum_mag_derivation.py
@@ -8,17 +8,17 @@ phi_dot = diff(phi,t)
 l = 1
 mu_0 = 1
 M_p = 1
-g = 1
-alpha = 0.1
+g = 10
+alpha = 0.5
 
 x_p = l * cos(phi)
 y_p = l * sin(phi)
 
 x_1 = 1
-y_1 = 1
+y_1 = -1
 
-m_p = Matrix([cos(phi), sin(phi)])
-m_1 = 1*Matrix([1,1])
+m_p = 1*Matrix([cos(phi), sin(phi)])
+m_1 = 0.5*Matrix([1,0])
 
 r = Matrix([ x_p - x_1, y_p - y_1 ])
 r_norm = sqrt( r[0]**2 + r[1]**2 )
@@ -34,12 +34,12 @@ L = simplify(T - V)
 
 # %%
 
-sol = simplify(Eq(diff(L, phi) - diff(diff(L, phi_dot),t), 0))
+sol = simplify(Eq(diff(L, phi) - diff(diff(L, phi_dot),t), alpha*phi_dot))
 sol
 # %%
 phi_t_t = solve(sol, diff(phi,t,t))[0]
 
 # %%
 
-phi_t_t_func = lambdify(phi, phi_t_t, modules="numpy")
+phi_t_t_func = lambdify([phi, phi_dot], phi_t_t, modules="numpy")
 # %%
--- pendulum_mag_sim.py
+++ pendulum_mag_sim.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 from pendulum_mag_derivation import phi_t_t_func
 
 def f(t, y):
-    return np.array([y[1], phi_t_t_func(y[0])])
+    return np.array([y[1], phi_t_t_func(y[0], y[1])])
 
 ts = np.linspace(0,30,500)
-sol = solve_ivp(f, (0,30), np.array([np.pi/4,0]), t_eval=ts)
+sol = solve_ivp(f, (0,30), np.array([0,0]), t_eval=ts)
 phis  = sol.y[0]
 
 xs = np.cos(phis)
